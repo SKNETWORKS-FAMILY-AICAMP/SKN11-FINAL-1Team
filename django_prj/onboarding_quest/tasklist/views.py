60d2bd2c81f5b5f7201374f70d53a9f7ef015b7a
Dawn Surplus <tjdghwls777@naver.com>
2025-07-04 14:00:51 +0900

    페이지 연결

--- django_prj/onboarding_quest/tasklist/views.py
+++ django_prj/onboarding_quest/tasklist/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'tasklist/tasklist.html')
+    return render(request, 'tasklist/task_add.html')
 
 # Create your views here.
